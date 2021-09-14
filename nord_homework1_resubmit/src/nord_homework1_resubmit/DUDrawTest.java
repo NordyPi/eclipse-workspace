3a136b6d32a963fbb954c89aa23faabc12c34e36
NordyPi <loklinlegos@gmail.com>
2021-09-14 11:23:37 -0600

    Fixed first HW assignment

---
+++ nord_homework1_resubmit/src/nord_homework1_resubmit/DUDrawTest.java
@@ -0,0 +1,13 @@
+package nord_homework1_resubmit;
+
+import edu.du.dudraw.DUDraw;
+
+public class DUDrawTest {
+
+	public static void main(String[] args) {
+		DUDraw.setPenColor(255, 0, 255);
+		DUDraw.filledCircle(0.25, 0.25, 0.3);
+
+	}
+
+}
---
+++ nord_homework1_resubmit/src/nord_homework1_resubmit/HelloWorld.java
@@ -0,0 +1,10 @@
+package nord_homework1_resubmit;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
