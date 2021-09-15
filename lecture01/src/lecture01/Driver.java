465413b53f4072a6c34512675b7d9aeb95c9f598
Loklin Nord <20norlok@student.mcpsmt.org>
2021-09-15 14:06:54 -0600

    Before coding configuration

--- lecture01/src/lecture01/Driver.java
+++ lecture01/src/lecture01/Driver.java
@@ -6,9 +6,8 @@ import java.util.Random;
 
 //Worked with Luke Clements to create and fix clock
 
-
 public class Driver {
-	
+
 	public static void main(String[] args) {
 		LocalTime now = LocalTime.now();
 		Random random = new Random();
@@ -21,30 +20,30 @@ public class Driver {
 		double minutey;
 		double hourx;
 		double houry;
-		
+
 		DUDraw.enableDoubleBuffering();
 		while (true) {
-			//Set up angles
+			// Set up angles
 			now = LocalTime.now();
 			second = (now.getSecond() / 60.0) * 2 * Math.PI;
-			minute = (now.getMinute() / 60.0) * 2 * Math.PI;	
+			minute = (now.getMinute() / 60.0) * 2 * Math.PI;
 			hour = ((now.getHour() % 12) / 12.0) * 2 * Math.PI;
-			
-			//Calculate coordinates
+
+			// Calculate coordinates
 			secondx = 0.5 + (Math.sin(second) * 0.28);
 			secondy = 0.5 + (Math.cos(second) * 0.28);
 			minutex = 0.5 + (Math.sin(minute) * 0.25);
 			minutey = 0.5 + (Math.cos(minute) * 0.25);
 			hourx = 0.5 + (Math.sin(hour) * 0.20);
 			houry = 0.5 + (Math.cos(hour) * 0.20);
-			
+
 			DUDraw.clear();
-			//Draw clock hands
+			// Draw clock hands
 			DUDraw.setPenColor((int) random.nextInt(255), (int) random.nextInt(255), (int) random.nextInt(255));
 			DUDraw.line(0.5, 0.5, secondx, secondy);
 			DUDraw.line(0.5, 0.5, minutex, minutey);
 			DUDraw.line(0.5, 0.5, hourx, houry);
-			//Clock outline
+			// Clock outline
 			DUDraw.circle(0.5, 0.5, 0.3);
 			DUDraw.show();
 		}
