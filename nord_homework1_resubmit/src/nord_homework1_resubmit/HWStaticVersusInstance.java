ac9e56d1c3e023c041aacfa804095ec03e91aefc
Loklin Nord <20norlok@student.mcpsmt.org>
2021-09-20 18:22:00 -0600

    Messingn with test class

---
+++ nord_homework1_resubmit/src/nord_homework1_resubmit/HWStaticVersusInstance.java
@@ -0,0 +1,14 @@
+	package nord_homework1_resubmit;
+
+
+public class HWStaticVersusInstance  {
+        private int myInteger;
+    
+        public HWStaticVersusInstance() {
+            myInteger = 0;
+        }
+
+        public static void myMethod() {
+            myInteger++;
+        }
+}
