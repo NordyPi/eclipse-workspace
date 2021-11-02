8e198c047044e37e0722fa80e749393ee628fce3
NordyPi <loklinlegos@gmail.com>
2021-11-02 14:12:47 -0600

    updated lab 14

--- NordLab14/src/nordlab14/EncoderDriver.java
+++ NordLab14/src/nordlab14/EncoderDriver.java
@@ -4,12 +4,12 @@ public class EncoderDriver {
 
 	public static void main(String[] args) {
 		ShiftCipher shift = new ShiftCipher(3);
+		System.out.println("facetiously with shift of 3");
 		System.out.println(shift.encode("facetiously"));
-		ShiftCipher decodeShift = new ShiftCipher(-3);
-		System.out.println(decodeShift.encode("idfhwlrxvo|"));
 		
-		ShuffleCipher shuffle = new ShuffleCipher(1);
-		System.out.println(shuffle.encode("abcdefghi"));
+		ShuffleCipher shuffle = new ShuffleCipher(3);
+		System.out.println("facetiously with suffle of 3");
+		System.out.println(shuffle.encode("facetiously"));
 	}
 
 }
