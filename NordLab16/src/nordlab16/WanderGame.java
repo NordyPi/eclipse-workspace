367872a25001b24dcef0ab32fc699c301e119858
Loklin Nord <20norlok@student.mcpsmt.org>
2021-11-08 19:54:13 -0700

    started lab 16

---
+++ NordLab16/src/nordlab16/WanderGame.java
@@ -0,0 +1,62 @@
+package nordlab16;
+
+import java.awt.Color;
+
+import edu.du.dudraw.Draw;
+import edu.du.dudraw.DrawListener;
+
+public class WanderGame implements DrawListener {
+	private double xPos;
+	private double yPos;
+	private double size;
+	private Color color;
+	
+	@Override
+	public void keyPressed(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(char arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(double arg0, double arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(double arg0, double arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(double arg0, double arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(double arg0, double arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
