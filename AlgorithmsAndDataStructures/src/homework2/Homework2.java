c07f72d64f704fd10936994fac5dae8227ad0218
NordyPi <loklinnord77@gmail.com>
2023-01-22 13:00:13 -0700

    finished hw 2

--- AlgorithmsAndDataStructures/src/homework2/Homework2.java
+++ AlgorithmsAndDataStructures/src/homework2/Homework2.java
@@ -8,6 +8,7 @@ public class Homework2 {
 		long totalTime = 0;
 		//runs for number of trials
 		for (int z = 0; z < trials; z++) {
+			System.out.println(z);
 			//creates the random array
 			int nums[] = new int[n];
 			/* FOR RANDOM NUMS
@@ -15,13 +16,13 @@ public class Homework2 {
 				int temp = (int)(Math.random() * n);
 				nums[i] = temp;
 			}*/
-			// for in order nums
+			 //for in order nums
 			for (int i = 0; i < n; i++) {
 				nums[i] = i;
 			}
 			//does time stuff
 			long startTime = System.currentTimeMillis();
-			int out[] = InsertionSort(nums);
+			MergeSort(nums, 0, n-1);
 			long endTime = System.currentTimeMillis();
 			totalTime += (endTime - startTime);
 		}
@@ -44,17 +45,53 @@ public class Homework2 {
 		return array;
 	}
 	
-	public static int[] MergeSort(int array[], int p, int r) {
+	public static void MergeSort(int array[], int p, int r) {
 		if (p < r) {
-			int q = (p + r) / 2;
+			int q = p + (r - p) / 2;
 			MergeSort(array, p, q);
 			MergeSort(array, q + 1, r);
 			Merge(array, p, q, r);
 		}
 	}
 	
-	public static int[] Merge(int array[], int p, int q, int r) {
-		
-	}
+    public static void Merge(int arr[], int p, int q, int r)
+    {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[p + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[q + 1 + j];
+ 
+        int i = 0, j = 0;
+        int k = p;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
 
 }
